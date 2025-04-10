9872769f9a51a112ac87a4f7a5d71c124a8b6678
vshivam05 <sv699291@gmail.com>
2025-04-10 16:21:56 +0530

    Start

---
+++ XPublicHoliday/src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Github User Info Fetcher</h1>
+    </>
+  );
+}
+
+export default App;
